1cf913ee2f3bb819c0e96b562ce8172f747065f1
John Cox <john.cox@wyome.com>
2012-04-01 07:38:50 -0400

    Added long form if / foreach calls for readability in templates.  Also, changed to HTML5 doctype to shorten some of the calls.  Moved scripts to bottom of the file.

--- application/views/language/edit_lang_file.php
+++ application/views/language/edit_lang_file.php
@@ -1,73 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<!DOCTYPE html>
+<html lang="en">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 	<meta http-equiv="cache-control" content="no-cache"/>
 	<link href="<?php echo base_url();?>css/style.css" type="text/css" rel="stylesheet"/>
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
-	<script type="text/javascript">
-	$(document).ready(function(){
-		$("#add_new_key").click(function() { ///add click action for button
-			var n = $('.ex_key').length+1; ///get lenght of new keys and values
-			var m = $('.ex_val').length+1;
-			//<![CDATA[
-			$('<div class="row"><label><?php echo $this->lang->line('language_key');?> : </label><input class="ex_key" type="text" name="new_key_'+n+'" size="30" /><br/><label><?php echo $this->lang->line('language_translation');?>:</label><input class="ex_val" type="text" name="new_value_'+m+'" size="89" /><br/><div class="clear"></div></div>').appendTo('#extra-lang'); ///add input for new key
-			//]]>
-		});
-		$('#create_file_form').hide();
-		$("#new_file").click(function() { ///add click action for button
-			$('#create_file_form').toggle();
-		});
-		$('#new_lang_form').hide();
-		$("#new_lang").click(function() { ///add click action for button
-			$('#new_lang_form').toggle();
-		});
-		$('.button_del').click(function(){
-			var answer = confirm('<?php echo $this->lang->line('language_confirm_lang_delete');?>');
-			return answer; // answer is a boolean
-		});
-		$('.delete_key').click(function(){
-			var answer = confirm('<?php echo $this->lang->line('language_confirm_key_delete');?>');
-			if(answer===true){
-				var key_name = $(this).attr('id');
-				$.ajax({
-					type: "POST",
-					dataType: 'json',
-					url: "<?php echo site_url('/language/remove_key');?>",
-					data: {'key':$(this).attr('id'),'filename':'<?php echo $file;?>','language':'<?php echo $language;?>'<?php echo ($this->config->item('csrf_protection') === TRUE) ? ', \'csrf_test_name\':\''.$this->security->get_csrf_hash().'\'' : '';?>},
-					success: function(msg){
-						if(msg.response==true){
-							var del = key_name.substr(4);
-							$("#row_"+del).remove();
-							//<![CDATA[
-							$("#results").append('<div class="msg">'+msg.msg+'</div>');
-							//]]>
-						}else if(msg.response==false){
-							//<![CDATA[
-							$("#results").append('<div class="error">'+msg.msg+'</div>');
-							//]]>
-						}
-					}
-				});
-			}else{
-				return FALSE;
-			}
-		});
-	});
-	</script>
+
 </head>
 <body>
 	<div id="wrapper">
-		<?php if($this->session->flashdata('error')){ ?>
+		<?php if($this->session->flashdata('error')): ?>
 			<div class="error">
 				<?php echo $this->session->flashdata('error');?>
 			</div>
-		<?php }elseif($this->session->flashdata('msg')){ ?>
+		<?php elseif($this->session->flashdata('msg')): ?>
 			<div class="msg">
 				<?php echo $this->session->flashdata('msg');?>
 			</div>
-		<?php } ?>
+		<?php endif; ?>
 		<div class="box menu">
 			<a href="<?php echo site_url('/language');?>"><?php echo $this->lang->line('language_home_link');?></a>&nbsp;|&nbsp;<a href="<?php echo site_url('/language/lang_list/'.$language);?>"><?php echo $this->lang->line('language_file_list_link');?></a>&nbsp;|&nbsp;<a href="#" id="new_file"><?php echo $this->lang->line('language_create_file_link');?></a>
 			<div class="right"><?php echo $this->lang->line('language_lang_label');?>: <strong><?php echo $language;?></strong> <?php echo $this->lang->line('language_file_label');?>: <strong><?php echo $file;?></strong></div>
@@ -82,19 +32,19 @@
 			</div>
 		</div>
 		<div class="left">
-			<?php if(isset($dir)&&!empty($dir)){
+			<?php if(isset($dir)&&!empty($dir)):
 				$this->load->view('language/dir_list_view');
-			 } ?>
+			 endif; ?>
 			<div class="clear"></div>
-			<?php if(!empty($dir)&&count($dir)>1){ ?>
+			<?php if(!empty($dir)&&count($dir)>1): ?>
 			<div class="box language listyle">
 				<p><b><?php echo $this->lang->line('language_edit_file_info');?></b></p>
 				<ul>
-				<?php foreach($dir as $d){
-					if($d['dir']!=$language){?>
-						<?php if(file_exists(APPPATH."language/{$d['dir']}/$file")){ ?>
+				<?php foreach($dir as $d):
+					if($d['dir']!=$language):?>
+						<?php if(file_exists(APPPATH."language/{$d['dir']}/$file")): ?>
 							<li><a href="<?php echo site_url('/language/lang_file/'.$d['dir'].'/'.$file);?>"><?php echo $d['dir'];?></a></li>
-						<?php }else{ ?>
+						<?php else: ?>
 							<li class="no-float"><?php echo $this->lang->line('language_langfile_not_exist').$d['dir'];?><br/>
 								<?php echo form_open(site_url('/language/create_file'));?>
 									<input type="hidden" name="filename" value="<?php echo $file;?>" />
@@ -103,14 +53,14 @@
 									<input type="submit" name="create" value="<?php echo $this->lang->line('language_create_label');?>"/>
 								</form>
 							</li>
-						<?php }
-					}
-				} ?>
+						<?php endif;
+					endif;
+				endforeach; ?>
 				</ul>
 			</div>
 			<?php } ?>
 		</div>
-		<?php if($keys===FALSE && !empty($lang)){ ?>
+		<?php if($keys===FALSE && !empty($lang)): ?>
 		<div class="box files">
 			<div><?php echo $this->lang->line('language_first_time_info');?></div>
 			<?php echo form_open(site_url('/language/update_all_keys'));?>
@@ -122,8 +72,8 @@
 			</form>
 			<p class="error"><?php echo $this->lang->line('language_first_time_warning');?></p>
 		</div>
-		<?php }
-		if(isset($extra_keys) && !empty($extra_keys)){ ?>
+		<?php endif;
+		if(isset($extra_keys) && !empty($extra_keys)): ?>
 			<div class="box files">
 				<div>
 				<p><?php echo $this->lang->line('language_keys_db_warning');?></p>
@@ -139,7 +89,7 @@
 				</div>
 				<div id="extra_keys" style="display:none;">
 					<ul>
-						<?php foreach($extra_keys as $k){ ?>
+						<?php foreach($extra_keys as $k): ?>
 							<li>
 								<?php echo $k;?> = "<?php echo $lang[$k];?>"
 								<?php echo form_open(site_url('/language/add_one_key'));?>
@@ -151,16 +101,16 @@
 									</div>
 								</form>
 							</li>
-						<?php } ?>
+						<?php endforeach; ?>
 					</ul>
 				</div>
 			</div>
-		<?php } ?>
+		<?php endif; ?>
 		<div class="box files">
 		<?php if($this->config->item('comments')==1) { ?><a href="#" onclick="$('.comments').toggle();"><?php echo $this->lang->line('language_sh_comments');?></a><br/><?php } ?>
 			<?php echo form_open(site_url('/language/save_language_file'));?>
 			<div id="results"></div>
-			<?php if(isset($keys)&&!empty($keys)) {?>
+			<?php if(isset($keys)&&!empty($keys)) :?>
 				<?php foreach($keys as $key) :?>
 					<div class="row" id="row_<?php echo $key;?>">
 						<label class="left"><?php echo $key;?> <?php if(is_array($lang)&&!array_key_exists($key,$lang)){ ?><small>(NEW!)</small><?php }?> : </label>
@@ -177,7 +127,7 @@
 						<div class="clear"></div>
 					</div>
 				<?php endforeach; ?>
-			<?php } ?>
+			<?php endif; ?>
 			<div id="extra-lang"></div>
 			<div class="clear"></div>
 			<br/>
@@ -191,5 +141,56 @@
 			Created by <a href="http://codebusters.pl/en">Eliza Witkowska</a>
 		</div>
 	</div>
+	
+    <script>
+    $(document).ready(function(){
+        $("#add_new_key").click(function() { ///add click action for button
+            var n = $('.ex_key').length+1; ///get lenght of new keys and values
+            var m = $('.ex_val').length+1;
+            //<![CDATA[
+            $('<div class="row"><label><?php echo $this->lang->line('language_key');?> : </label><input class="ex_key" type="text" name="new_key_'+n+'" size="30" /><br/><label><?php echo $this->lang->line('language_translation');?>:</label><input class="ex_val" type="text" name="new_value_'+m+'" size="89" /><br/><div class="clear"></div></div>').appendTo('#extra-lang'); ///add input for new key
+            //]]>
+        });
+        $('#create_file_form').hide();
+        $("#new_file").click(function() { ///add click action for button
+            $('#create_file_form').toggle();
+        });
+        $('#new_lang_form').hide();
+        $("#new_lang").click(function() { ///add click action for button
+            $('#new_lang_form').toggle();
+        });
+        $('.button_del').click(function(){
+            var answer = confirm('<?php echo $this->lang->line('language_confirm_lang_delete');?>');
+            return answer; // answer is a boolean
+        });
+        $('.delete_key').click(function(){
+            var answer = confirm('<?php echo $this->lang->line('language_confirm_key_delete');?>');
+            if(answer===true){
+                var key_name = $(this).attr('id');
+                $.ajax({
+                    type: "POST",
+                    dataType: 'json',
+                    url: "<?php echo site_url('/language/remove_key');?>",
+                    data: {'key':$(this).attr('id'),'filename':'<?php echo $file;?>','language':'<?php echo $language;?>'<?php echo ($this->config->item('csrf_protection') === TRUE) ? ', \'csrf_test_name\':\''.$this->security->get_csrf_hash().'\'' : '';?>},
+                    success: function(msg){
+                        if(msg.response==true){
+                            var del = key_name.substr(4);
+                            $("#row_"+del).remove();
+                            //<![CDATA[
+                            $("#results").append('<div class="msg">'+msg.msg+'</div>');
+                            //]]>
+                        }else if(msg.response==false){
+                            //<![CDATA[
+                            $("#results").append('<div class="error">'+msg.msg+'</div>');
+                            //]]>
+                        }
+                    }
+                });
+            }else{
+                return FALSE;
+            }
+        });
+    });
+    </script>
 </body>
 </html>
